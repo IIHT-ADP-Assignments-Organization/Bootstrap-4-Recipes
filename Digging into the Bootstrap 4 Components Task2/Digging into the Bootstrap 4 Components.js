d4b667a4585af25b27cda52a7a5cd58e0cff4fcd
itdev-lenovo <it1devops@gmail.com>
2020-04-07 19:21:33 +0530

    files added

---
+++ Digging into the Bootstrap 4 Components Task2/Digging into the Bootstrap 4 Components.js
@@ -0,0 +1,11 @@
+//1).Using Bootstrap 4, create and dismiss the alert message for successfully submitting the details for a Contact //Us form using Javascrpit
+ 
+
+<html>
+<h1>Digging into the Bootstrap 4 Components<h1>
+<Script>
+</Script>
+<html>
+
+
+
